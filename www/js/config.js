d1c54f1d0dcc6cb0e696ea7788b437019dc18c18
Rafael Lategahn <rafael@lategahn.eng.br>
2017-11-01 15:19:25 -0200

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+angular.module('your_app_name.config', [])
+//.constant('WORDPRESS_API_URL', 'https://wordpress.startapplabs.com/blog/api/')
+.constant('WORDPRESS_API_URL', 'http://www.einfach.com.br/wp_app_imuno/api/')
+.constant('WORDPRESS_PUSH_URL', 'https://wordpress.startapplabs.com/blog/push/')
+.constant('GCM_SENDER_ID', '574597432927')
+
+;
